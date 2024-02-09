e8de8dfc24cd70e0361ba7a6bf2444085d4d45f3
Adighost135 <92373537+Adighost135@users.noreply.github.com>
2024-02-09 20:22:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function yes(){
+    var img = document.getElementById("pfp");
+    img.src = "images/happycat.gif";
+}
+
+function no(){
+    var img = document.getElementById("pfp");
+    img.src = "images/sadcat.jpg";
+}
